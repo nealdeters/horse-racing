096d10fbf98986f5aca47f600c1827054d7c59c7
Neal Deters <nealdeters@gmail.com>
2020-05-16 16:59:40 -0500

    updated user capability schema

--- database/models/usercapability.js
+++ database/models/usercapability.js
@@ -1,6 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const UserCapability = sequelize.define('UserCapability', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
     userId: {
     	type: DataTypes.INTEGER,
 	    references: {
